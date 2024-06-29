b841e97b560732cca223ff77364335cb869ed098
RajClement <rajgclement@gmail.com>
2024-06-29 18:25:04 +0530

    Data_Ingestion

--- src/exception.py
+++ src/exception.py
@@ -24,11 +24,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-
-# Example usage
-if __name__ == "__main__":
-    try:
-        # Simulate an error for demonstration purposes
-        1 / 0
-    except Exception as e:
-        raise CustomException(e, sys)
